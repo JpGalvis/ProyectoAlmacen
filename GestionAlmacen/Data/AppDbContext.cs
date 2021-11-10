271d32e52b947d7c7a3aabb5a48b3645b0ec8cb1
jgalvissaavedra <jgalvissaavedra@gmail.com>
2021-11-10 17:16:33 -0400

    app

---
+++ GestionAlmacen/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GestionAlmacen.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionAlmacen.Data
+{
+    public class AppDbContext : DbContext
+
+    {
+        public DbSet<Almacenes> Almacen { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
+            
+
+    }
+}
