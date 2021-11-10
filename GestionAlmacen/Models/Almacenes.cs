42010c13708b9ef481b8bcb2a54b33ca8daaab70
jgalvissaavedra <jgalvissaavedra@gmail.com>
2021-11-10 16:55:55 -0400

    clase models

--- GestionAlmacen/Models/Almacenes.cs
+++ GestionAlmacen/Models/Almacenes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace GestionAlmacen.Models
 {
     public class Almacenes
     {
+        [Key]
+        public string producto { get; set; }
+        [Display(Name = "nombre del producto")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "El nombre debe contener entre 3 a 60 caracteres")]
+        [Required(ErrorMessage = "producto requerido")]
+        public string almacen { get; set; }
+        [Display(Name = "tipo de almacen")]
+        public string informacion { get; set; }
+        [Url]
+        [Display(Name = "link")]
+        public string link { get; set; }
+
     }
 }
